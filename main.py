7cf4d47484c64efa826ebbc762e2b7bc9fe68c34
Rohaan Rehman <>
2021-05-27 00:15:20 +0000

    starting the coding

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from tkinter import *
 
-Class Main
+class MainScreen:
+    def __init__ (self, master):
+        background_color = "black"
+
+        self.main_frame = Frame(master, bg = background_color, padx=100, pady=100)
+        self.main_frame.grid()
+
+        self.Heading_label = Label(self.main_frame, )
+
 
 if __name__ == "__main__":
     root = Tk()
-    root.titile("Quiz on Global Pollution")
+    root.title("Quiz on Global Pollution")
     root.mainloop()
